3a7a2d46a670688f20ac54a5a4ada1fd4474d376
UndeX <volodin.bi@yandex.ru>
2022-11-17 16:25:18 +0300

    Update

---
+++ bot.py
@@ -0,0 +1,27 @@
+import os
+from datetime import datetime as dt
+import discord
+from discord.ext import commands
+
+TOKEN = os.getenv('TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+TIME_SK = 0
+TIME_SH = 0
+TIME_ORG = 0
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    # for guild in client.guilds:
+    #     if guild.name == GUILD:
+    #         break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+    
+    
+client.run(TOKEN)
